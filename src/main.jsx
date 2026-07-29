de64d489d536778bb214591932255248aa5ff813
Deepak Pathak <dp662971@gmail.com>
2026-07-29 08:08:58 +0530

    Scaffold lead dashboard as deployable Vite React app

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LeadDashboard from "./LeadDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LeadDashboard />
+  </React.StrictMode>
+);
